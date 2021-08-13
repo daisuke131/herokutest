1eccc43123962d61ab49098f0ccb03063f1d0e2f
daisuke <dada.00131@gmail.com>
2021-08-13 15:35:01 +0900

    run修正

--- run.py
+++ run.py
@@ -1,11 +1,19 @@
+from concurrent.futures import ThreadPoolExecutor
+
 from common.driver import Driver
 
 
 def main():
+    with ThreadPoolExecutor() as executor:
+        for i in range(10):
+            executor.submit(driver_dr, i)
+
+
+def driver_dr(i):
     dr = Driver()
     dr.get("https://www.amazon.co.jp/")
     dr.quit()
-    print("class成功")
+    print(f"{i+1}回目成功")
 
 
 if __name__ == "__main__":
